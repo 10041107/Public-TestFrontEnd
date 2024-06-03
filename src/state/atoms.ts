2ab08c8141426a73da6cc5d83ab902d6f8074287
10041108 <aksmfdyfl111@gmail.com>
2024-06-03 21:23:06 +0900

    allReset

--- src/state/atoms.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-import { QuizResult } from '../pages/publics/quiz/result'; 
-
-export const quizResultState = atom<QuizResult | null>({
-  key: 'quizResultState',
-  default: null,
-});
